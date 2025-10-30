e80b5b55d2dba92b9c3652b54ce410cc3a388cf8
rafakka <rafaelkaher@gmail.com>
2025-10-30 12:48:02 -0300

    setting up relashionship done

--- db_manager.py
+++ db_manager.py
@@ -1,22 +1,35 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy import Column, Engine, Integer, String, Float
+from sqlalchemy import create_engine, Column, Engine, Integer, String, Float, Table, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship, sessionmaker
 
 engine = create_engine("sqlite:///recipes.db", echo=True)
-
 Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+
+class RecipeIngredient(Base):
+    __tablename__ ="recipe_ingredients"
+
+    recipe_id = Column(Integer, ForeignKey("recipes.id"), primary_key=True)
+    ingredient_id=Column(Integer, ForeignKey("ingredients.id"), primary_key=True)
+    quantity = Column(Float)
+
+    recipe = relationship("Recipe", back_populates="recipe_ingredients")
+    ingredient=relationship("Ingredient",back_populates="ingredient_recipes")
+
+class Recipe(Base):
+    __tablename__="recipes"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    steps = Column(String)
+    recipe_ingredients = relationship("RecipeIngredient", back_populates="ingredients")
 
 class Ingredient(Base):
     __tablename__="ingredients"
         
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
-    quantity = Column (Float, default=0)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
     unit = Column(String)
+    ingredient_recipes = relationship("RecipeIngredient", back_populates="ingredients")
 
-    def __repr__(self):
-        return f"<Ingredient(name='{self.name}',qty={self.quantity},unit='{self.unit})>"
-
-SessionLocal = sessionmaker(bind=engine)
 
 Base.metadata.create_all(bind=Engine)
--- recipes_manager.py
+++ recipes_manager.py
@@ -1,5 +1,14 @@
-def add_recipe(name, ingredient, steps):
-    #todo
-def find_recipes_with(avaliable_ingredients):
-    #todo
-    
+from db_manager import SessionLocal, Recipe, Ingredient
+
+def add_recipe(name, steps, ingredient_data):
+    session = SessionLocal()
+    recipe = Recipe(name=name, steps=steps)
+
+    for data in ingredient_data:
+        ingredient = session.query(ingredient).filter_by(name=data["name"]).first()
+        if not ingredient:
+            ingredient = Ingredient(name=data["name"], unit=data["unit"])
+            recipe.ingredients.append(ingredient)
+        session.add(recipe)
+        session.commit()
+        session.close()

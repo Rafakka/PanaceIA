f1a922b4021586044d7d0f5b7051e2300ba8e680
rafakka <rafaelkaher@gmail.com>
2025-10-31 08:15:59 -0300

    update ingredients manager to dict

--- ingredients_manager.py
+++ ingredients_manager.py
@@ -1,7 +1,12 @@
 from db_manager import SessionLocal, Ingredient
 
-def add_ingredient(name, quantity, unit):
+def add_ingredient(ingredient_data:dict):
+
     session = SessionLocal()
+    name = ingredient_data.get("name") 
+    quantity = ingredient_data.get("quantity") 
+    unit = ingredient_data.get("unit")
+
     ingredients = Ingredient(name=name,quantity=quantity,unit=unit)
     session.add(ingredients)
     session.commit()
@@ -13,17 +18,36 @@ def list_ingredients():
     session.close()
     return ingredients
 
-def update_quantity(name, new_quantity):
+def update_ingredient_name(ingredient_data:dict):
     session = SessionLocal()
-    ingredient = session.query(Ingredient).filter_by(name=name).first()
+
+    old_name = ingredient_data.get("old_name") 
+    new_name = ingredient_data.get("new_name")
+
+    ingredient = session.query(Ingredient).filter_by(name=old_name).one_or_none()
+    if ingredient:
+        ingredient.name = new_name
+        session.commit()
+    session.close()
+
+def update_ingredient_quantity(ingredient_data:dict):
+    session = SessionLocal()
+
+    name = ingredient_data.get("name") 
+    new_quantity = ingredient_data.get("new_quantity")
+
+    ingredient = session.query(Ingredient).filter_by(name=name).one_or_none()
     if ingredient:
         ingredient.quantity = new_quantity
         session.commit()
     session.close()
-    
-def remove_ingredient(name):
+
+def remove_ingredient(ingredient_data:dict):
     session = SessionLocal()
-    ingredient = session.query(Ingredient).filter_by(name=name).first()
+
+    name = ingredient_data.get("name")
+
+    ingredient = session.query(Ingredient).filter_by(name=name).one_or_none()
     if ingredient:
         session.delete(ingredient)
         session.commit()
